3cb105cf9dc362921cdcc6e4082fdbdd1b78fabe
Pritanshu69 <samantaprit1924@gmail.com>
2025-09-03 20:10:35 +0530

    temp of config

---
+++ config.py
@@ -0,0 +1,18 @@
+WORDS = [
+    "hello",
+    "help",
+    "yes",
+    "no",
+    "please",
+    "thank you",
+]
+
+SEQUENCE_LENGTH = 30
+SAMPLES_PER_WORD = 40
+
+#NOTE - THESE ARE THE PATHS
+DATA_DIR = "data"
+MODEL_DIR = "models"
+
+CONFIDENCE_THRESHOLD = 0.8
+STABILITY_WINDOW = 5
